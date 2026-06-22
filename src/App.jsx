cee41251e3348d9847bfd7e6574b7df31035f681
Jessica Ferreira <fjes.ks@gmail.com>
2026-06-22 18:22:44 -0300

    Validations

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,63 @@ import './App.css';
 
 function App(){
   const [formData, setFormData] = useState({
-    name: '',
-    email: ''
+        name: '',
+        email: ''
+      });
+    
+      const [errors, setErrors] = useState({});
 
-  });
+
+  const validate = () => {
+    const newErrors = {};
+    if(!formData.name.trim()){
+      newErrors.name = 'Name is required';
+    }
+    return newErrors;
+  };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length >0){
+      setErrors(validationErrors);
+    } else {
+      console.log('Form data Submitted', formData);
+    }
     console.log('Form Data Submitted', formData);
   };
 
   const handleChange =  (e) => {
+    const {name,value} = e.target;
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+
+    if (errors[name]){
+      const newErrors = {...errors};
+      delete newErrors [name];
+      setErrors(newErrors); 
+    }
   };
 
   return (
     <div>
       <h1>Forms in React</h1>
       <form onSubmit = {handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <span style ={{color: 'red'}} >{errors.name}</span>}
+          <br/>
+        </label>
+        
         <label>
           Email:
           <input
