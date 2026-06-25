304e23cf785f9128066029ddbb9bfd6aefcaeb6f
Jessica Ferreira <fjes.ks@gmail.com>
2026-06-25 11:13:48 -0300

    validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React,{useEffect} from "react";
 import './App.css';
 import {useForm} from 'react-hook-form';
 
 
 function App(){
-  const {register, handleSubmit, watch} = useForm();
+  const {register, handleSubmit, watch,  formState: {errors}} = useForm();
   const onSubmit =(data) => console.log();
 
   //console.log(watch('name'));
@@ -26,13 +26,24 @@ function App(){
       <form onSubmit= {handleSubmit(onSubmit)}>
         <label>
           Name:
-          <input {...register('name')}/>
+          {/* <input {...register('name', {required: true, minLength:2})}/> */}
+          <input {...register('name',
+             {required: 'Name is required',
+              minLength: { value: 2,
+                          message: 'Name should be atleast 2 characters'
+
+              }})}
+              />
+
         </label>
+        {errors.name && <p>{errors.name.message}</p>}
         
         <label>
           Email:
-          <input{...register('email')}/>
+          <input{...register('email', {required: true, pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ })}/>
         </label>
+        {errors.email && <p>Email is required</p>}
+
 
         <button type = 'submit'>Submit</button> 
       </form>
